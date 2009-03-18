8599e452f541bacf44781f318115de50b145c325
tef <tef@vorrat.(none)>
2009-03-18 04:16:29 +0000

    initial commit of pycurl skeleton

---
+++ fetch.py
@@ -0,0 +1,124 @@
+# library around pycurl and it's barebones c-like interface
+
+import StringIO
+import re
+
+from urlparse import urlparse,urlunparse
+
+import pycurl
+
+
+__all__ = [ "fetch", "Session" ]
+
+def fetch(resource, **options):
+    s = Session()
+    return s.fetch(resource, **options)
+
+
+class Session(object):
+    """A session contains defaults for fetching resources, 
+    as well as keeping the http referer"""
+    def __init__(self, **options):
+        self.curl = pycurl.Curl()
+        self.options = options
+
+    def fetch(self, resource, **options):
+        opts = self.options.copy()
+        opts.update(options)
+        opts = self.curl_setup(opts)
+
+        response = StringIO.StringIO()
+        headers = StringIO.StringIO()
+
+        self.curl.setopt(pycurl.WRITEFUNCTION, response.write)
+        self.curl.setopt(pycurl.HEADERFUNCTION, headers.write)
+
+        opts = self.fetch_setup(resource, opts)
+
+        self.curl.perform()
+
+        response_code = self.curl.getinfo(pycurl.RESPONSE_CODE)
+        content_type = self.curl.getinfo(pycurl.CONTENT_TYPE)
+
+        charset = opts.pop("charset",None)
+
+        if not charset and content_type:
+                charset = extract_charset(content_type) 
+
+        location = self.curl.getinfo(pycurl.EFFECTIVE_URL)
+        
+        return {
+            'raw_data': response.getvalue(),
+            'raw_headers' : headers.getvalue(),
+            'charset': charset,
+            'url': location,
+            }
+
+    def fetch_setup(self, resource, options):
+        curl = self.curl
+        
+        get = options.pop("get", None)
+        if get:
+            url = resource + "?" + url_encode(get)
+        else:
+            url = resource
+
+        curl.setopt(pycurl.URL, url)
+
+        post = options.pop("post", None)
+        if post:
+            post = url_encode(post)
+
+            curl.setopt(pycurl.POSTFIELDS, post)
+            curl.setopt(pycurl.POST,1)
+        else:
+            curl.setopt(pycurl.HTTPGET,1)
+
+        return options
+
+
+
+    def curl_setup(self, options):
+        curl = self.curl
+        proxy = options.pop("proxy",None)
+        if proxy:
+            curl.setopt(pycurl.PROXY,proxy)
+
+        verify_ssl = options.pop("verify_ssl",0)
+        verify_ssl = int(bool(verify_ssl))
+        curl.setopt(pycurl.SSL_VERIFYPEER,verify_ssl)
+
+        user_agent = options.pop("user_agent",False)
+        if user_agent is None:
+            curl.setopt(pycurl.USERAGENT,"")
+        elif user_agent:
+            curl.setopt(pycurl.USERAGENT,user_agent)
+
+        gzip = options.pop("gzip",True)
+        if gzip:
+            curl.setopt(pycurl.ENCODING,"gzip, deflate")
+        else:
+            curl.setopt(pycurl.ENCODING,"")
+
+        curl.setopt(pycurl.FOLLOWLOCATION, 1)
+
+        return options
+
+    
+rx = re.compile("charset=([\w-]+)", re.U|re.I)
+
+def extract_charset(header):
+    o = rx.search(header)
+    if o:
+        return o.groups()[0]
+    else:
+        return None
+    
+
+def url_encode(data):
+    if isinstance(dict, data):
+        return urlencode(data)
+    elif isinstance(unicode, data):
+        return data.encode("utf-8")
+    else:
+        return str(data)

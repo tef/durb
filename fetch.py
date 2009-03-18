63c0a16d98df157aea4393b617dd01193c4eb5f2
tef <tef@vorrat.(none)>
2009-03-18 04:35:07 +0000

    adding response object

--- fetch.py
+++ fetch.py
@@ -4,9 +4,9 @@ import StringIO
 import re
 
 from urlparse import urlparse,urlunparse
+from urllib import urlencode
 
 import pycurl
-
 
 __all__ = [ "fetch", "Session" ]
 
@@ -14,10 +14,10 @@ def fetch(resource, **options):
     s = Session()
     return s.fetch(resource, **options)
 
-
 class Session(object):
     """A session contains defaults for fetching resources, 
     as well as keeping the http referer"""
+
     def __init__(self, **options):
         self.curl = pycurl.Curl()
         self.options = options
@@ -47,12 +47,13 @@ class Session(object):
 
         location = self.curl.getinfo(pycurl.EFFECTIVE_URL)
         
-        return {
+        return Response({
+            'response_code' : response_code,
             'raw_data': response.getvalue(),
-            'raw_headers' : headers.getvalue(),
-            'charset': charset,
+            'data_charset': charset,
+            'headers' : decode_headers(headers.getvalue()),
             'url': location,
-            }
+            })
 
     def fetch_setup(self, resource, options):
         curl = self.curl
@@ -75,8 +76,6 @@ class Session(object):
             curl.setopt(pycurl.HTTPGET,1)
 
         return options
-
-
 
     def curl_setup(self, options):
         curl = self.curl
@@ -104,6 +103,21 @@ class Session(object):
 
         return options
 
+class Response(dict):
+    def text(self, charset=None):
+        if charset is None:
+            charset = self['data_charset']
+        return unicode(self['raw_data'].decode(charset,"xmlcharrefreplace"))
+
+def decode_headers(headers_text):
+    headers = []
+    for line in headers_text.splitlines():
+        if line.find(":") > 0:
+            name, value = line.split(":",1)
+            headers.append((name,value))
+        else:
+            headers.append(line)
+    return headers
     
 rx = re.compile("charset=([\w-]+)", re.U|re.I)
 
@@ -116,9 +130,9 @@ def extract_charset(header):
     
 
 def url_encode(data):
-    if isinstance(dict, data):
+    if isinstance(data, dict):
         return urlencode(data)
-    elif isinstance(unicode, data):
+    elif isinstance(data, unicode):
         return data.encode("utf-8")
     else:
         return str(data)

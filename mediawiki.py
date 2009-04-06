1c121d68b215d6b4415cd79d00f037fe7802e88a
tef <tef@vorrat.(none)>
2009-04-06 16:00:52 +0100

    fixing for new mediawiki api

--- mediawiki.py
+++ mediawiki.py
@@ -28,21 +28,24 @@ class Session(object):
 
     def get_edit_token(self, page_title):
         r = self._fetch(action="query", prop="info|revisions", intoken="edit", titles=page_title)
-        token = r.xpath('/api/query/pages/page[@title = "%s"]/@edittoken'%page_title)[0]
-        last_rev= r.xpath('/api/query/pages/page[@title = "%s"]/@touched'%page_title)[0]
+        token = r.xpath('/api/query/pages/page[@title = "%s"]/@edittoken'%page_title)
+        token = token[0] if len(token) > 0 else None
+        last_rev= r.xpath('/api/query/pages/page[@title = "%s"]/@touched'%page_title)
+        last_rev = last_rev[0] if len(last_rev) > 0 else None
 
         return (token, last_rev)
 
     def edit(self,token, title, text):
         edit_token, timestamp = token
-
-        r = self._fetch(
+        args = dict(
             action="edit",
             title=title, 
             text=text,
-            token=edit_token,
-            starttimestamp=timestamp
-            )
+            token=edit_token
+        )
+        if timestamp:
+            args["starttimestamp"]=timestamp
+        r = self._fetch(**args)
 
         return etree.tostring(r)
 
--- test_mw.py
+++ test_mw.py
@@ -9,7 +9,7 @@ def loadpw(filename):
 un,pw = loadpw("passwd")
 print un,pw
 def mw():
-    a = mediawiki.Session("http://www.includipedia.com/mediawiki/api.php")
+    a = mediawiki.Session("http://www.includipedia.com/mediawiki-1.14.0/api.php")
     print a.login(un,pw)
     token = a.get_edit_token("Includipedia:TestPageOnly")
     print a.edit(token,"Includipedia:TestPageOnly","thisisatest")

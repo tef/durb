442bcfbeda70a4fa1004b5d385236c8f9f02201d
tef <tef@vorrat.(none)>
2009-04-06 16:04:22 +0100

    minor tweaks

--- test_mw.py
+++ test_mw.py
@@ -7,18 +7,18 @@ def loadpw(filename):
     return lines.split(":",1)
 
 un,pw = loadpw("passwd")
-print un,pw
-def mw():
+def mw(text):
     a = mediawiki.Session("http://www.includipedia.com/mediawiki-1.14.0/api.php")
     print a.login(un,pw)
     token = a.get_edit_token("Includipedia:TestPageOnly")
-    print a.edit(token,"Includipedia:TestPageOnly","thisisatest")
+    print a.edit(token,"Includipedia:TestPageOnly",text)
     print a.logout()
 
-def wp():
+def wp(text):
     a = mediawiki.Session("http://en.wikipedia.org/w/api.php")
     token = a.get_edit_token("Wikipedia:Sandbox")
     print  a.edit(token,"Wikipedia:Sandbox","thisisatest")
 
-
-mw()
+import sys
+if __name__ == '__main__':
+    mw("".join(str(x) for x in sys.argv[1:]))

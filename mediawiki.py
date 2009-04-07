a85ca95fc4bd0fe989d5ebf7948326b90dbf4a64
tef <tef@vorrat.(none)>
2009-04-07 18:05:56 +0100

    making a better command line tool

--- mediawiki.py
+++ mediawiki.py
@@ -14,32 +14,92 @@ parser.add_option("-T", "--title", dest="title", help="title of page")
 parser.add_option("-t", "--text", dest="text", help="text of page")
 parser.add_option("-f", "--file", dest="file", help="file with text")
 parser.add_option("-A", "--auth", dest="auth", metavar="FILE",help="file with auth")
+parser.add_option("-U", "--url", dest="url",help="mediawiki url")
 
 default_auth = os.path.expanduser("~/.mediawiki")
 mw_url = "http://www.includipedia.com/mediawiki-1.14.0/api.php"
 
 def main(argv):
-    command = argv.pop(0).lower()
+    if not argv[0].startswith("-"):
+        action = argv.pop(0).lower()
+    else:
+        action = None
+
     (options, args) = parser.parse_args(argv)
-    c = Command(command,options, args)
+    url = options.url or mw_url
+    auth_file = options.auth or default_auth
+    un, pw = read_creds(auth_file, url)
+
     try:
-        c.run()
+        s = Session(url)
+        if action == "login":
+            un,pw = ask_creds()
+            if s.login(un,pw):
+                write_creds(auth_file, un, pw, url)
+        else:
+            if un and pw:
+                if not s.login(un,pw):
+                    raise Exception, "failed to login"
+            if action == "edit":
+                text = options.text
+                if not text:
+                    file = options.file 
+                    if file and file != "-":
+                        fh = open(file, "r")
+                        text = "".join(fh.readlines())
+                        fh.close()
+                    else:
+                        text = "".join(sys.stdin.readlines())
+                    
+                title = options.title or "Includipedia:TestPageOnly"
+                token = s.get_edit_token(title)
+                s.edit(token, title, text)
+
+        s.logout()
     except BaseException, e:
         sys.stderr.write("error: %s\n" % e)
+
+
+def read_creds(filename, url):
+    """Given a url, read the credentials for it"""
+    try:
+        fh = open(filename,"r")
+    except:
+        return None, None
     
+    un, pw = None,None
+    for line in fh.readlines():
+        un_, pw_, url_ = line.strip().split(":",2)
+        if url == url_:
+            un, pw = un_, pw_
+            break
+    fh.close()
+    return un,pw
 
-class Command(object):
-    def __init__(self, command, options, args):
-        self.options = options
-        self.args = args
-        self.run = getattr(self,command)
-        s = Session(mw_url)
+def write_creds(filename, un,pw, url):
+    """write the new credentials into the savefile, preserving existing creds that do not overlap"""
+    try:
+        fh = open(filename,"r")
+        creds = fh.readlines()
+        fh.close()
+    except:
+        creds = []
 
-    def login(self):
-        print "username:"
+    fh = open(filename,"w")
+    fh.write("%s:%s:%s\n"%(un,pw,url))
+    for line in creds:
+        un_, pw_, url_ = line.strip().split(":",2)
+        if url != url_:
+            fh.write("%s:%s:%s\n"%(un_,pw_,url_))
+    fh.close()
+
+
+def ask_creds():
+        """prompt the user for credentials"""
+        print "username:",
         user = raw_input()
         pw = getpass.getpass("password:")
-
+        return user,pw
 
 class Session(object):
     """A session object handles the api connection and provides
